9087599284ef1309182c644577823e8a422ed137
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2021-10-12 16:57:25 +0300

    Add Table class

---
+++ Table.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DBMS {
+    public class Table {
+        public string Name { get; set; }
+        public List<Row> Rows { get; set; }
+        public List<Column> Columns { get; set; }
+    }
+}
