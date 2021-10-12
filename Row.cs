99e20e92c8d1f3f407888fd1ad8e13c936ea5566
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2021-10-12 16:49:42 +0300

    Add Row class

---
+++ Row.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMS {
+    public class Row {
+        public List<object> Values { get; set; }
+
+        public object this[int i] {
+            get => Values[i];
+            set => Values[i] = value;
+        }
+    }
+}
